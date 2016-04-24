3cd654f5117c28b2a9a78bd78deb30d0b4d8b311
dengshilong <dengshilong1988@gmail.com>
2016-04-24 22:43:02 +0800

    fix: archive view

--- date.php
+++ date.php
@@ -34,13 +34,28 @@
 		
 		<?php } ?>				
 		<br/>				
-		<?php foreach ($posts as $post) : start_wp(); ?>				
-			<?php require('post.php'); ?>
-		<?php endforeach; ?>
-		<p align="center"><?php posts_nav_link() ?></p>		
-		<?php } else { ?>
-			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>			
-		<?php } ?>			
+            <?php if ( have_posts() ) : ?>
+                <?php
+                // Start the loop.
+                while ( have_posts() ) : the_post();
+                    /*
+                     * Include the Post-Format-specific template for the content.
+                     * If you want to override this in a child theme, then include a file
+                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                     */
+                    get_template_part( 'content', get_post_format() );
+
+                // End the loop.
+                endwhile;
+                // If no content, include the "No posts found" template.
+                else :
+                    get_template_part( 'content', 'none' );
+                endif;
+            ?>
+        <p align="center"><?php posts_nav_link() ?></p>     
+        <?php } else { ?>
+            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>           
+        <?php } ?>
 	</div>
 	<div id="sidebar">
 		<?php /* If this is a daily archive */ if (isset($_GET['day']) && !empty($_GET['day'])) { ?>
